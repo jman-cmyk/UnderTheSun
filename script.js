b53a858e3c640e61cca297b0bf89c43333b927de
jman-cmyk <jmorasch25@my.whitworth.edu>
2023-11-22 22:28:14 -0800

    Update

--- script.js
+++ script.js
@@ -41,7 +41,27 @@ function createStars() {
 }
 
 
+function updateProgressBarAndTimer() {
+    const targetDate = new Date('2023-12-15T08:00:00');
+    const now = new Date();
+    const timeLeft = targetDate - now;
 
+    // Update progress bar
+    const totalDuration = targetDate - new Date('2023-01-01T00:00:00'); // Assuming the start date is January 1, 2023
+    const progress = ((totalDuration - timeLeft) / totalDuration) * 100;
+    document.getElementById('progress-bar').style.width = `${progress}%`;
+
+    // Update countdown timer
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    document.getElementById('days').textContent = days;
+    document.getElementById('hours').textContent = hours;
+    document.getElementById('minutes').textContent = minutes;
+    document.getElementById('seconds').textContent = seconds;
+}
 
 
 function twinkleStar(star) {
